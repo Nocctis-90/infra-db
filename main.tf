cff060604d94ffb77e01b9f496a5a692d97999bc
caiorodrigues <caioc.rodrigues@live.com>
2024-03-19 00:03:58 -0300

    feat: config infra db

--- main.tf
+++ main.tf
@@ -1,32 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
 
-  required_version = ">= 1.2.0"
-}
 
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_db_instance" "example" {
+resource "aws_db_instance" "default" {
   allocated_storage    = 20
-  storage_type         = "gp2"
+  db_name              = "fasfoodproducaodb"
   engine               = "postgres"
-  engine_version       = "12.6"
-  instance_class       = "db.t2.micro"
-  db_name              = "example-db"
-  username             = "admin"
-  password             = "suaSenhaSegura123"  # Substitua pela sua senha
-  parameter_group_name = "default.postgres12"
-  publicly_accessible  = false
-
-  tags = {
-    Name = "ExampleDB"
-  }
-} 
-
+  engine_version       = "15.4"
+  instance_class       = "db.t3.micro"
+  username             = teste
+  password             = teste124
+}
