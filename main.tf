03d2f4ae637f6621e8029449b67bc131fb3c19fb
caiorodrigues <caioc.rodrigues@live.com>
2024-03-18 22:59:23 -0300

    feat: alterando main.tf

--- main.tf
+++ main.tf
@@ -1,55 +1,19 @@
 provider "aws" {
-  region = "us-east-1"  # substitua pela sua região desejada
-}
+  region = "us-east-1"  
 
-resource "aws_db_instance" "meu_banco_de_dados" {
-  # Configurações do seu banco de dados
-  instance_class = "db.t2.micro"
-  engine         = "mysql"
-  username       = "usuario"
-  password       = "senha"
-  allocated_storage = 20
-  identifier     = "meu-banco-de-dados"
-}
+resource "aws_db_instance" "example" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "postgres"
+  engine_version       = "12.6"
+  instance_class       = "db.t2.micro"
+  name                 = "example-db"
+  username             = "admin"
+  password             = "suaSenhaSegura123"  # Substitua pela sua senha
+  parameter_group_name = "default.postgres12"
+  publicly_accessible  = false
 
-resource "aws_iam_role" "meu_role" {
-  name = "meu-role-rds"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "rds.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy" "minha_politica" {
-  name        = "minha-politica-rds"
-  description = "Permite acesso ao banco de dados RDS"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "rds-db:connect"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "minha_politica_attachment" {
-  role       = aws_iam_role.meu_role.name
-  policy_arn = aws_iam_policy.minha_politica.arn
+  tags = {
+    Name = "ExampleDB"
+  }
 }
