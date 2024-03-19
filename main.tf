19ed3b302bd69ad4f60189e728d590e3ea1dd89d
Nocctis-90 <munhoz.raul90@gmail.com>
2024-03-18 23:26:34 -0300

    mudando a versão do hashicorp

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.66.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1"  # substitua pela sua região desejada
 }
