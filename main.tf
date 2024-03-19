7dc06709b4f2f3e4cfa0dd8cc373de11ef9a3be1
caiorodrigues <caioc.rodrigues@live.com>
2024-03-18 23:31:10 -0300

    feat: inclusao do provider

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.41.0"
-    }
-  }
+provider "aws" {
+  region = us-east-1
+  profile = default
 }
 
 resource "aws_db_instance" "example" {
